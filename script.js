16c5ef8a7a0c7a133dd729507d94d01a9fe89c8d
Fiorusss <giordano.fioretti1993@gmail.com>
2024-11-27 15:11:39 +0100

    weather app finita

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon")
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    var data = await response.json();
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else{
+        var data = await response.json();
    
     
     document.querySelector(".city").innerHTML = data.name;
@@ -26,6 +31,11 @@ async function checkWeather(city) {
     }
 
     document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    }
+      
+
+    
 }
 
 searchBtn.addEventListener("click", ()=>{
